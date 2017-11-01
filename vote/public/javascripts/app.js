4888a91fd596ef7418a833306b1e589ea7289213
lily514 <l.hamelaswood@gmail.com>
2017-11-01 22:12:35 +0000

    Kaylee in costume. Add works

---
+++ vote/public/javascripts/app.js
@@ -0,0 +1,45 @@
+var app = window.angular.module('app', [])
+
+app.factory('costumeFetcher', costumeFetcher)
+app.controller('mainCtrl', mainCtrl)
+
+
+function costumeFetcher ($http) {
+  
+  var API_ROOT = 'costume'
+  return {
+    get: function () {
+      return $http
+        .get(API_ROOT)
+        .then(function (resp) {
+          return resp.data
+        })
+    }
+  }
+
+}
+
+function mainCtrl ($scope, costumeFetcher, $http) {
+
+  $scope.costume = []
+  costumeFetcher.get()
+    .then(function (data) {
+      $scope.costume = data
+    })
+
+$scope.addCostume = function() {
+  var formData = {name:$scope.Name,avatarUrl:$scope.Url};
+  console.log(formData);
+  var costumeURL = 'costume';
+  $http({
+     url: costumeURL,
+     method: "POST",
+     data: formData
+  }).success(function(data, status, headers, config) {
+    console.log("Post worked");
+  }).error(function(data, status, headers, config) {
+    console.log("Post failed");
+  });
+}
+  
+}
--- vote/routes/index.js
+++ vote/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var costumes = [
+  {
+    name: 'Kaylee',
+    avatarUrl: 'https://scontent.fmkc1-1.fna.fbcdn.net/v/t31.0-8/22904854_10155814706393245_8910946532594894102_o.jpg?oh=0df921752c815efe1da129e7cc940fed&oe=5AA4E1F8'
+  },
+];
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.sendFile('index.html', { root: 'public' });
 });
+router.get('/costume', function(req, res) {
+  console.log("In Costumes");
+res.send(costumes);
+
+
+});
+
+router.post('/costume', function(req, res) {
+    console.log("In Costmes Post");
+    console.log(req.body);
+    costumes.push(req.body);
+    res.end('{"success" : "Updated Successfully", "status" : 200}');
+});
+
+
 
 module.exports = router;
